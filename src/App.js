c8f92090bd001a6c0250581dc142fa76bd12891e
Pavel Kochkin <Pavel.Kochkin@inventry.co.uk>
2022-01-21 10:49:34 +0300

    baby im

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs'
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+
+  const dialogsData = [
+    { id: "1", name: "Alex" },
+    { id: "2", name: "Miha" },
+    { id: "3", name: "Toha" },
+    { id: "4", name: "Kroshka" }
+  ]
+
+  const messagesData = [
+    { message: "Hi" },
+    { message: "Hello" },
+    { message: "Go sex" },
+    { message: "WTF?!??!?" }
+  ]
+
+
   return (
-      <div className='app-wrapper'>
-        <Header />
-        <Navbar />
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
 
-        <div className='app-wrapper-content'>
-          <Routes>
-            <Route path="/dialogs" element={<Dialogs />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </div>
-
+      <div className='app-wrapper-content'>
+        <Routes>
+          <Route path="/dialogs" element={<Dialogs dialogsData={dialogsData} messagesData={messagesData} />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
       </div>
+
+    </div>
   );
 }
 
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,25 +2,9 @@ import css from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogItem'
 import Message from './Message/Message'
 
-const Dialogs = () => {
-
-    const dialogsData = [
-        { id: "1", name: "Alex" },
-        { id: "2", name: "Miha" },
-        { id: "3", name: "Toha" },
-        { id: "4", name: "Kroshka" }
-    ]
-
-    const dialogElements = dialogsData.map(dialog => <DialogItem id={dialog.id} name={dialog.name} key={dialog.id}></DialogItem>)
-
-    const messagesData = [
-        { message: "Hi" },
-        { message: "Hello" },
-        { message: "Go sex" },
-        { message: "WTF?!??!?" }
-    ]
-
-    const messageElements = messagesData.map(msg => <Message message={msg.message} key={msg.message}></Message>)
+const Dialogs = (props) => {
+    const dialogElements = props.dialogsData.map(dialog => <DialogItem id={dialog.id} name={dialog.name} key={dialog.id}></DialogItem>)
+    const messageElements = props.messagesData.map(msg => <Message message={msg.message} key={msg.message}></Message>)
 
     return (
         <div className={css.dialogs}>
