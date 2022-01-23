b57f88912400540767f577597f1e11fdbd82e735
Pavel Kochkin <Pavel.Kochkin@inventry.co.uk>
2022-01-23 18:36:22 +0300

    bam

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,7 @@ import Post from '../Post/Post'
 const MyPosts = (props) => {
 debugger;
     
-    const postElements =  props.data.posts.map(post => <Post postText={post}></Post> )
+    const postElements =  props.posts.map(post => <Post postText={post}></Post> )
 
     return(
         <div>My posts
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,13 @@
 import MyPosts from './MyPosts/MyPosts.jsx'
 import css from './Profile.module.css'
 const Profile = (props) => {
+    debugger;
     return (
         <div>
             <div>
                 avatar+description
             </div>
-            <MyPosts data={props.data}/>
+            <MyPosts posts={props.data.posts}/>
             <img className={css.avatar} src="https://vetliva.ru/upload/resize_cache/iblock/582/1170_640_2ad00f77cb8d50abfaeeae09efd4a5122/5824e36662a84f1350fcc661fe0ef9ee.jpg" />
         </div>)
 }
